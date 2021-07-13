82675749d7158d00606a0515d80e8b31e188511f
Kevin Jump <kevin@thejumps.co.uk>
2021-07-13 20:02:34 +0100

    Include build folder.

---
+++ build/build.ps1
@@ -0,0 +1,11 @@
+param($version, $suffix, $env='release', [switch]$push=$false)
+
+$fullVersion = -join($version, '-', $suffix)
+$outFolder = ".\$fullVersion"
+$packageProj = "..\src\Our.Umbraco.LinkedPages\Our.Umbraco.LinkedPages.csproj"
+
+dotnet pack $packageProj -c $env -o $outFolder /p:ContinuousIntegrationBuild=true,version=$fullVersion
+
+if ($push) {
+    .\nuget.exe push "$outFolder\*.nupkg" -ApiKey AzureDevOps -src https://pkgs.dev.azure.com/jumoo/Public/_packaging/nightly/nuget/v3/index.json
+}
