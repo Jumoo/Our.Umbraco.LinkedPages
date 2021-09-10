b6977e28e4677d6120da6fdfe3296bb6abf8b5fa
Kevin Jump <kevin@thejumps.co.uk>
2021-09-10 20:33:54 +0100

    update to v9-rc003

--- src/LinkedPages.CoreSite/Startup.cs
+++ src/LinkedPages.CoreSite/Startup.cs
@@ -62,8 +62,8 @@ namespace LinkedPages.CoreSite
             app.UseUmbraco()
                 .WithMiddleware(u =>
                 {
-                    u.WithBackOffice();
-                    u.WithWebsite();
+                    u.UseBackOffice();
+                    u.UseWebsite();
                 })
                 .WithEndpoints(u =>
                 {
